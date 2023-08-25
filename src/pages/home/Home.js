915b5d658c9f93f8d1c6799ce4074e9d27ab83b3
okikiola12 <88362706+okikiola12@users.noreply.github.com>
2023-08-25 03:16:07 +0100

    first intiation

---
+++ src/pages/home/Home.js
@@ -0,0 +1,18 @@
+import './Home.css'
+import HorizoSlider from '../../components/HorizoSlider'
+
+
+
+
+const Home = () => {
+  return (
+    <>
+
+      <HorizoSlider/>
+
+
+    </>
+  )
+}
+
+export default Home
